5598af69c551789c257c70fd18880f2de143b3ed
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-15 13:36:02 +0100

    creare i files

---
+++ script.js
@@ -0,0 +1,12 @@
+/* //^ESERCIZIO LISTA SPESA
+& Data una lista della spesa già compilata nel vostro codice, stampare in console gli elementi della lista individualmente, prima con un ciclo for, poi con un ciclo while.
+
+Quando le stampe in console sono corrette, stampare gli elementi in un elenco puntato in HTML, utilizzando uno dei due cicli scritti al punto precedente.
+
+~ Bonus:
+Aggiungere alla pagina un campo di input testuale e un pulsante “aggiungi”: l’utente inserisce nell’input un prodotto da acquistare e quando clicca sul pulsante, il testo viene aggiunto alla lista.
+
+~Super Bonus:
+Aggiungere a fianco ad ogni elemento della lista un pulsante “elimina”: quando l’utente clicca su uno di questi pulsanti, l’elemento corrispondente viene rimosso dalla lista
+*/ 
+
