1e8d806b4b38572202201e8b928ed8f59d277456
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-15 13:49:12 +0100

    stampare gli elementi con for e while loop

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* //^ESERCIZIO LISTA SPESA
-& Data una lista della spesa già compilata nel vostro codice, stampare in console gli elementi della lista individualmente, prima con un ciclo for, poi con un ciclo while.
+Data una lista della spesa già compilata nel vostro codice, stampare in console gli elementi della lista individualmente, prima con un ciclo for, poi con un ciclo while.
 
 Quando le stampe in console sono corrette, stampare gli elementi in un elenco puntato in HTML, utilizzando uno dei due cicli scritti al punto precedente.
 
@@ -10,3 +10,16 @@ Aggiungere alla pagina un campo di input testuale e un pulsante “aggiungi”: 
 Aggiungere a fianco ad ogni elemento della lista un pulsante “elimina”: quando l’utente clicca su uno di questi pulsanti, l’elemento corrispondente viene rimosso dalla lista
 */ 
 
+let expenses = ["pizza", "farina", "yogurt", "limone", "mele", "latte", "mozzarella", "avocado", "pomodorini"];
+
+//* stampare gli elementi della lista con for loop
+for(let i =0; i<expenses.length; i++){
+    console.log(expenses[i]);
+}
+
+//*stampare gli elementi della lista con while loop
+let i = 0;
+while(i<expenses.length){
+console.log(expenses[i]);
+i++
+}
